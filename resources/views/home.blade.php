54e74f46be6a1c32e826a6a27fd6265da02ff8fb
valero.geoffrey06@gmail.com <valero.geoffrey06@gmail.com>
2017-05-02 15:22:36 +0200

    obiwan init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <h3 class="card-header">Featured</h3>
+                <div class="card-block">
+                    <h4 class="card-title">Special title treatment</h4>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/layouts/proto.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
+
+<div class="container">
+    <hr>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <h3 class="card-header">Featured</h3>
+                <div class="card-block">
+                    <h4 class="card-title">Special title treatment</h4>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    Header
+                </div>
+                <div class="card-block">
+                    <h4 class="card-title">Header</h4>
+                    <p class="card-text">Header</p>
+                </div>
+                <div class="card-footer text-muted">
+                    Header
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
